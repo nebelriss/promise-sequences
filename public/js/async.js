e0ef2879554009559f8934303de65a5cac5d7494
Michel Heiniger <git@akiramail.com>
2018-10-11 22:44:39 +0200

    Added some comments

--- public/js/async.js
+++ public/js/async.js
@@ -4,39 +4,45 @@
 
     let sequence = Promise.resolve();
 
-    animals.animalURLs.forEach((animal) => {
-      sequence = sequence.then(() => {
-        return getJSON(animal).then(addTextToDOM);
-      });
+    // animals.animalURLs.forEach((animal) => {
+    //   sequence = sequence.then(() => {
+    //     return getJSON(animal).then(addTextToDOM);
+    //   });
+    // });
+
+    /*
+    * Same as above but jetzt written out
+    */
+    // sequence = sequence.then(() => {
+    //   return getJSON(animals.animalURLs[0]).then(addTextToDOM);
+    // });
+    // sequence = sequence.then(() => {
+    //   return getJSON(animals.animalURLs[1]).then(addTextToDOM);
+    // });
+    // sequence = sequence.then(() => {
+    //   return getJSON(animals.animalURLs[2]).then(addTextToDOM);
+    // });
+    // sequence = sequence.then(() => {
+    //   return getJSON(animals.animalURLs[3]).then(addTextToDOM);
+    // });
+    // sequence = sequence.then(() => {
+    //   return getJSON(animals.animalURLs[4]).then(addTextToDOM);
+    // });
+
+    /*
+    * How the sequence looks like after the loop
+    * /
+    sequence.then(() => {
+      return getJSON(animals.animalURLs[0]).then(addTextToDOM);
+    }).then(() => {
+      return getJSON(animals.animalURLs[1]).then(addTextToDOM);
+    }).then(() => {
+      return getJSON(animals.animalURLs[2]).then(addTextToDOM);
+    }).then(() => {
+      return getJSON(animals.animalURLs[3]).then(addTextToDOM);
+    }).then(() => {
+      return getJSON(animals.animalURLs[4]).then(addTextToDOM);
     });
-
-    // sequence = sequence.then(() => {
-    //   return getJSON(animals.animalURLs[0]).then(addTextToDOM);
-    // });
-    // sequence = sequence.then(() => {
-    //   return getJSON(animals.animalURLs[1]).then(addTextToDOM);
-    // });
-    // sequence = sequence.then(() => {
-    //   return getJSON(animals.animalURLs[2]).then(addTextToDOM);
-    // });
-    // sequence = sequence.then(() => {
-    //   return getJSON(animals.animalURLs[3]).then(addTextToDOM);
-    // });
-    // sequence = sequence.then(() => {
-    //   return getJSON(animals.animalURLs[4]).then(addTextToDOM);
-    // });
-
-    // sequence.then(() => {
-    //   return getJSON(animals.animalURLs[0]).then(addTextToDOM);
-    // }).then(() => {
-    //   return getJSON(animals.animalURLs[1]).then(addTextToDOM);
-    // }).then(() => {
-    //   return getJSON(animals.animalURLs[2]).then(addTextToDOM);
-    // }).then(() => {
-    //   return getJSON(animals.animalURLs[3]).then(addTextToDOM);
-    // }).then(() => {
-    //   return getJSON(animals.animalURLs[4]).then(addTextToDOM);
-    // });
 
   });
 })();

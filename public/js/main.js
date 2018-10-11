02001a7329c3352b1f5808031da83c6764a87192
Michel Heiniger <git@akiramail.com>
2018-10-11 16:56:10 +0200

    Added testing function

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,9 @@
 (function() {
     getJSON('/animals').then((animals) => {
         addHeadingToDOM(animals.title);
+
+        animals.animalURLs.forEach((animal) => {
+            console.log(animal);
+        });
     });
 })();

3ab759ca35fdc8d1f941b75e9c9a15191e8a4573
Michel Heiniger <git@akiramail.com>
2018-10-12 15:02:19 +0200

    changed comments

--- public/js/async.js
+++ public/js/async.js
@@ -4,11 +4,11 @@
 
     let sequence = Promise.resolve();
 
-    // animals.animalURLs.forEach((animal) => {
-    //   sequence = sequence.then(() => {
-    //     return getJSON(animal).then(addTextToDOM);
-    //   });
-    // });
+    animals.animalURLs.forEach((animal) => {
+      sequence = sequence.then(() => {
+        return getJSON(animal);
+      }).then(addTextToDOM);
+    });
 
     /*
     * Same as above but jetzt written out
@@ -32,17 +32,17 @@
     /*
     * How the sequence looks like after the loop
     */
-    sequence.then(() => {
-      return getJSON(animals.animalURLs[0]).then(addTextToDOM);
-    }).then(() => {
-      return getJSON(animals.animalURLs[1]).then(addTextToDOM);
-    }).then(() => {
-      return getJSON(animals.animalURLs[2]).then(addTextToDOM);
-    }).then(() => {
-      return getJSON(animals.animalURLs[3]).then(addTextToDOM);
-    }).then(() => {
-      return getJSON(animals.animalURLs[4]).then(addTextToDOM);
-    });
+    // sequence.then(() => {
+    //   return getJSON(animals.animalURLs[0]).then(addTextToDOM);
+    // }).then(() => {
+    //   return getJSON(animals.animalURLs[1]).then(addTextToDOM);
+    // }).then(() => {
+    //   return getJSON(animals.animalURLs[2]).then(addTextToDOM);
+    // }).then(() => {
+    //   return getJSON(animals.animalURLs[3]).then(addTextToDOM);
+    // }).then(() => {
+    //   return getJSON(animals.animalURLs[4]).then(addTextToDOM);
+    // });
 
   });
 })();
